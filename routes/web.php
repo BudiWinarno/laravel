18ebc41380c05416f494834b46468d071d807d84
BudiWinarno <budiwinarno2@gmail.com>
2024-11-12 11:09:40 +0700

    add login register fitur

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginRegisterController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -15,15 +16,11 @@ Route::delete('/produk/delete{id}', [HomeController::class, 'destroy'])->name('i
 Route::get('/produk/edit{id}', [HomeController::class, 'edit'])->name('index.edit');
 Route::put('/produk/update{id}', [HomeController::class, 'update'])->name('index.update');
 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/home', 'home')->name('home');
+    Route::post('/logout', 'logout')->name('logout');
 });
-
-require __DIR__.'/auth.php';
